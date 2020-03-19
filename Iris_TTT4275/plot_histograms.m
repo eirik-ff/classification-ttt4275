608d6dd3c97d16052d66c27f84c179aae7f9e109
Eirik Falck <eirik.falck@gmail.com>
2020-03-19 17:30:42 +0100

    add script to plot histograms to iris task 2a

---
+++ Iris_TTT4275/plot_histograms.m
@@ -0,0 +1,61 @@
+%% Load data
+class1 = load('class_1');  % Setosa
+class2 = load('class_2');  % Versicolour
+class3 = load('class_3');  % Virginica
+
+all_data = [class1; class2; class3];
+
+%% Defs
+sl_bins = min(all_data(:,1)):0.1:max(all_data(:,1)); % sepal length
+sw_bins = min(all_data(:,2)):0.1:max(all_data(:,2)); % sepal width
+pl_bins = min(all_data(:,3)):0.1:max(all_data(:,3)); % petal length
+pw_bins = min(all_data(:,4)):0.1:max(all_data(:,4)); % petal width
+
+
+%% Plot histogram sepal length
+figure(1);
+clf;
+
+subplot(411);
+title('Comparison of features across classes');
+
+hold on;
+histogram(class2(:,1), sl_bins);
+histogram(class1(:,1), sl_bins);
+histogram(class3(:,1), sl_bins);
+
+xlabel('Sepal length [cm]');
+ylabel('Count');
+
+
+subplot(412);
+hold on;
+histogram(class2(:,2), sw_bins);
+histogram(class1(:,2), sw_bins);
+histogram(class3(:,2), sw_bins);
+
+xlabel('Sepal width [cm]');
+ylabel('Count');
+
+
+subplot(413);
+hold on;
+histogram(class2(:,3), pl_bins);
+histogram(class1(:,3), pl_bins);
+histogram(class3(:,3), pl_bins);
+
+xlabel('Petal length [cm]');
+ylabel('Count');
+
+
+
+subplot(414);
+hold on;
+histogram(class2(:,4), pw_bins);
+histogram(class1(:,4), pw_bins);
+histogram(class3(:,4), pw_bins);
+
+xlabel('Petal width [cm]');
+ylabel('Count');
+
+legend('Setosa (class 1)', 'Versicolour (class 2)', 'Virginica (class 3)');
