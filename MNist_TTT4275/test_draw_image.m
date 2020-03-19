cfd97952704c12fd5ad81c23b72f900a1c1594dc
Eirik Falck <eirik.falck@gmail.com>
2020-03-19 18:31:38 +0100

    test if draw_image function works with subplots. conclusion: it does

--- MNist_TTT4275/test_draw_image.m
+++ MNist_TTT4275/test_draw_image.m
@@ -1,6 +1,20 @@
 % Test draw_image
+subplot(221);
+draw_image(testv(1,:), testlab(1));
 
-for i = 1:length(testv)
+subplot(222);
+draw_image(testv(2,:), testlab(2));
+
+subplot(223);
+draw_image(testv(3,:), testlab(3));
+
+subplot(224);
+draw_image(testv(4,:), testlab(4));
+
+
+pause;
+clf;
+for i = 1:10
     draw_image(testv(i,:), testlab(i));
     pause
 end
