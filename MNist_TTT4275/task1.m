f55288170ba2c0bcb0d7920ed3ad8cff0ebc3d0b
Eirik Falck <eirik.falck@gmail.com>
2020-03-26 15:24:12 +0100

    finish task1 of mnist

--- MNist_TTT4275/task1.m
+++ MNist_TTT4275/task1.m
@@ -6,39 +6,14 @@ N_chunks   = N_train / chunk_size;
 
 
 %% Classify
-% test defines
-N_chunks = 10;
-N_test = 1000;
-
 guess = zeros(10,N_test);
 tic;
 for i = 1:N_test
     test = testv(i,:);
-    
-    % Go through all chunks and find and save closest image in each chunk
-    dmin = Inf;
-    imin = 0;
-    for j = 0:N_chunks-1
-        first = j*chunk_size+1;
-        last  = (j+1)*chunk_size;
-        train = trainv(first:last,:);
-        dists = dist(train,test'); % chunk_size x 1
-        
-        [d,di] = min(dists); % di: distance index in this chunk
-        if d < dmin 
-            dmin = d;
-            imin = j*chunk_size + di;
-        end
-    end
-    
-    % Find the chunk with the closest image
-    pred = trainlab(imin);
+    dists = dist(train,test');
+    [d,di] = min(dists);
+    pred = trainlab(di);
     guess(pred+1,i) = 1;
-
-%     dists = dist(trainv,test');
-%     [d,di] = min(dists);
-%     pred = trainlab(di);
-%     guess(pred+1,i) = 1;
 end
 toc
 
