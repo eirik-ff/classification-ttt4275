6f357d64941564274d6f9af0c27fb5e1583a3377
Eirik Falck <eirik.falck@gmail.com>
2020-03-29 19:15:15 +0200

    implement draw_misclassified to go through all test results and draw a misclassified image in each categrory

---
+++ MNist_TTT4275/draw_misclassified.m
@@ -0,0 +1,28 @@
+% Results must be loaded as "known" and "guess"
+% data_all must be loaded
+
+%% Find misclassified
+misclassified_idx = NaN(10,1);
+for i = 1:length(guess)
+    g = find(guess(:,i));
+    k = find(known(:,i));
+    
+    if g ~= k && isnan(misclassified_idx(g))
+        misclassified_idx(g) = i;
+    end
+end
+
+%% Draw misclassified
+figure(1);
+clf;
+sgtitle('Some misclassified images');
+
+for i = 1:10
+    subplot(2,5,i);
+    idx = misclassified_idx(i);
+    draw_image(testv(idx,:));
+    title({sprintf('Guess %d',i-1),sprintf('Real: %d',testlab(idx))});
+end
+
+
+
